7d051442f485ed5eb6a8ecb4af5bf550a360c504
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-10-01 02:49:06 -0700

    update

--- kaggle_lib/pytorch/test.py
+++ kaggle_lib/pytorch/test.py
@@ -116,8 +116,7 @@ for limit in [2500, 2500, 2500, 2500, 2500]:
     print()
 """
 
-for num_workers, batch_size, limit in [(2,32,100000), (2,32,100000), (4,32,100000), (4,32,100000), (8,32,100000),
-                                       (8,32,100000)]:
+for num_workers, batch_size, limit in [(2,32,500), (2,32,10000), (4,32,10000), (8,32,10000)]:
     test(use_jdataloader=True, use_dataloader=False, use_joblib=False, small=False, N=10, limit=limit,
          batch_size=batch_size, num_workers=num_workers)
     print()
