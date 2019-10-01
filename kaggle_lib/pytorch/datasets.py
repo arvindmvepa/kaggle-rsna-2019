8d1298a23c249b8796b4ba8350982127f78f6748
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-10-01 01:28:29 -0700

    debug

--- kaggle_lib/pytorch/datasets.py
+++ kaggle_lib/pytorch/datasets.py
@@ -108,6 +108,8 @@ class RSNA2019Dataset(VisionDataset):
         if target is not None:
             output['target'] = target
 
+        print("index: {}, img shape: {}".format(index, output["image"]))
+
         return output
 
     def __len__(self):
