3d12da9639ef559005c140ae789b520ad960647b
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-10-01 14:18:15 -0700

    test

--- kaggle_lib/pytorch/test.py
+++ kaggle_lib/pytorch/test.py
@@ -117,7 +117,7 @@ for limit in [2500, 2500, 2500, 2500, 2500]:
 """
 
 for num_workers, batch_size, limit in [(2,32,300000), (2,32,300000), (4,32,300000), (8,32,300000)]:
-    test(use_jdataloader=True, use_dataloader=False, use_joblib=False, joblib_backend='multiprocessing',
+    test(use_jdataloader=True, use_dataloader=False, use_joblib=False, joblib_backend='loky',
          small=False, N=10, limit=limit, batch_size=batch_size, num_workers=num_workers)
     print()
     print()
